66f10b8f43ad1c5a8804fe7d5d3c6cfb8067bca7
Evgeniy Datskij <evgenij.datskij@mail.ru>
2024-01-11 09:12:56 +0500

    set server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/shared/ui/button";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>btn </Button>
+      <Button>button </Button>
     </main>
   );
 }
